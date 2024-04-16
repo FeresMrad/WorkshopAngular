c745ff039e918a0e6f07d1fc9e664c566eb14620
FeresMrad <mohamedferes.mrad@esprit.tn>
2024-04-16 09:27:28 +0100

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResidencesComponent } from './residences/residences.component';
+import { FormResidenceComponent } from './form-residence/form-residence.component';
+import { ApartmentsComponent } from './apartments/apartments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:ResidencesComponent},
+  {path:'addResidence',component:FormResidenceComponent},
+  {path: 'apartments/:residenceId', component: ApartmentsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResidencesComponent } from './residences/residences.component';
+import { FormsModule } from '@angular/forms';
+import { ApartmentsComponent } from './apartments/apartments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResidencesComponent,
+    ApartmentsComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
